93d1499e5c634803e7a9bba65af25acff919483b
Kaival <mailkaivalshah@gmail.com>
2024-06-07 16:30:24 +0530

    Debugging Deployment error

--- app.py
+++ app.py
@@ -69,8 +69,9 @@ def logout():
             quote_via=quote_plus,
         )
     )
-
+print("Before ENV")
 mongo_uri = os.getenv('MONGO_URI')
+print(mongo_uri)
 if mongo_uri is None:
     raise ValueError("MONGO_URI environment variable is not set")
 
